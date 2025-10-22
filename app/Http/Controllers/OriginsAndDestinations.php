f263bc290f7be7d29615dace98a03e115dcf31c8
Cril727 <criatiangarcia637@gmail.com>
2025-10-22 15:07:58 -0500

    add endpoints flight, my reservation

--- app/Http/Controllers/OriginsAndDestinations.php
+++ app/Http/Controllers/OriginsAndDestinations.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Destination;
+use App\Models\Flight;
 use App\Models\Origin;
 use Illuminate\Http\Request;
 
@@ -22,5 +23,19 @@ class OriginsAndDestinations extends Controller
         $cityOrigin = Destination::all();
 
         return response()->json(['success' => true, 'city' => $cityOrigin], 200);
+    }
+
+    public static function flightsByOrigin($origin_id)
+    {
+        $flights = Flight::where('origin_id', $origin_id)->get();
+
+        return response()->json(['success' => true, 'flights' => $flights], 200);
+    }
+
+    public static function flightsByDestination($destination_id)
+    {
+        $flights = Flight::where('destination_id', $destination_id)->get();
+
+        return response()->json(['success' => true, 'flights' => $flights], 200);
     }
 }
